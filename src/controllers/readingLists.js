f6de9d589f36d3721644c2cf602b1e8cc85e07e3
matej <matej.djerdji@gmail.com>
2024-07-08 19:56:50 +0200

    Exercise 13.22

--- src/controllers/readingLists.js
+++ src/controllers/readingLists.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+const { EntityNotFoundError } = require('../middleware/errorHandler')
+const { userAuthenticator } = require('../middleware')
 const { ReadingListBlog } = require('../models')
 
 router.post('/', async (req, res) => {
@@ -9,6 +11,30 @@ router.post('/', async (req, res) => {
     userId,
     blogId,
   })
+
+  res
+    .status(200)
+    .send(readingListEntry)
+})
+
+router.put('/:id', userAuthenticator, async (req, res) => {
+  const { id } = req.params
+  const { read } = req.body
+  
+  const readingListEntry = await ReadingListBlog.findByPk(id)
+  
+  if (!readingListEntry) throw new EntityNotFoundError()
+
+  if (req.user.id !== readingListEntry.userId) {
+    return res.status(403).end()
+  }
+
+  if (typeof read !== 'boolean') {
+    return res.status(400).end()
+  }
+
+  readingListEntry.read = read;
+  await readingListEntry.save()
 
   res
     .status(200)
